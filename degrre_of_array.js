28e048b24097db0f37eed4216cdabf6df9abc03f
laoye_jnr <idrisaboladeo@gmail.com>
2021-03-20 21:17:21 +0100

    all test passed on leetcode

--- degrre_of_array.js
+++ degrre_of_array.js
@@ -4,7 +4,10 @@ var findShortestSubArray = function (nums) {
 
     let first_seen = new Map()
     let min_length = 0
-
+//  *** Pseudo Code ****
+//  1. keep track of where eacj element occurs first
+//  2 keep track of the number of times each element occurs
+//  3 the differnece between the last index and the first index at which the element occur  should be the contigous subarray
     for (let i = 0; i <= nums.length; i++) {
         if (first_seen) {
             if (first_seen.get(nums[i]) === undefined) {
@@ -14,18 +17,23 @@ var findShortestSubArray = function (nums) {
                 first_seen.set(nums[i], first_seen.get(nums[i]))
             };
         }
-
         numCount.set(nums[i], (numCount.get(nums[i]) || 0) + 1)
+        
+
         if (numCount.get(nums[i]) > degree) {
             degree = numCount.get(nums[i])
-            // console.log(nums[i], '==>', i, 'first_seen @ ==>', first_seen.get(nums[i]));
-            // console.log('min_length ==>', i - first_seen.get(nums[i]) + 1 );
-            min_length = i - first_seen.get(nums[i])  + 1
+            min_length = i - first_seen.get(nums[i]) + 1
+        } else if (numCount.get(nums[i])=== degree) {
+            console.log('i got here');
+            min_length = Math.min(min_length, i - first_seen.get(nums[i]) + 1)
         }
     }
+
+    // console.log(numCount, first_seen);
     return 'min-length==>', min_length;
 };
 
-const subArray = findShortestSubArray([1, 2, 2, 3, 4, 2])
-
+const subArray = findShortestSubArray([2,1,1,2,1,3,3,3,1,3,1,3,2])
+// [2,1,1,2,1,3,3,3,1,3,1,3,2]
+// [1,2,2,3,1]
 console.log(subArray);
